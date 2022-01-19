74008af9e82274d4af94eaa296177f89caf64f1c
Benjamín Ayala <bayala0@ucol.mx>
2022-01-19 00:17:04 -0600

    feat(vars): var, let y const

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-
-
-
-
-
-
-console.log('Hola Mundo!!')
-
-
-
-
-
+// Var, Let y Const
+const nombre = "Tony";
+const apellido = "Stark";
+console.log({nombre, apellido});
+if( true ){
+	let nombre = "";
+		nombre = "Peter";
+	const apellido = "Parker";
+	console.log({nombre, apellido});
+	console.log({global});
+}
+var global = "Variable Global";
